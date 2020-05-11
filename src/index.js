e3303ec0e2b3e47da9d27f9cf8276e568e6bd7d3
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-12 02:30:25 +0300

    project init

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export class X {
-    constructor(x) {
-        this.x = x;
-    }
-
-    get foo() {
-        return this.x;
-    }
-}
--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai';
-
-describe('js :: lorem ipsum test suite', function() {
-    it('dolor sit amet test', function() {
-        expect(true).to.be.true;
-    });
-});
