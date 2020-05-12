fdc76ed13a1c50311edc2e2aa51cd0349eea3b03
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-13 01:52:56 +0300

    wrap all file tests into one describe() to skip easyer

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,108 +8,110 @@ import {expect} from 'chai';
 
 import {FileHandle, tempFile, tempFileOfSize} from '../src/index';
 
-describe('tempFile', function () {
-  let fh: FileHandle | undefined = undefined;
+describe('File', () => {
+  describe('tempFile', function () {
+    let fh: FileHandle | undefined = undefined;
 
-  // beforeEach(() => {});
+    // beforeEach(() => {});
 
-  afterEach(async () => {
-    if (fh !== undefined) {
-      fh.close();
-      await fs.promises.unlink(fh.name);
-      fh = undefined;
-    }
-  });
-
-  it(`calling tempFile() should create an empty file in ${os.tmpdir()}`, async () => {
-    const promise = tempFile();
-    expect(promise instanceof Promise).to.be.true;
-
-    fh = (await promise) as FileHandle;
-    const stat = await fs.promises.stat(fh.name);
-
-    expect(fh.name.indexOf(os.tmpdir())).to.equal(0);
-    expect(stat.isFile()).to.be.true;
-    expect(stat.size).to.equal(0);
-  });
-
-  it(`calling tempFile({}, callback) should create an empty file in ${os.tmpdir()}`, (done) => {
-    const promise = tempFile({}, (err: Error | null, handle?: FileHandle) => {
-      if (err) throw err;
-
-      expect(handle).to.be.an('object');
-
-      fh = handle;
-      /* eslint-disable @typescript-eslint/no-non-null-assertion */
-      fs.promises.stat(fh!.name).then((stat) => {
-        expect(fh!.name.indexOf(os.tmpdir())).to.equal(0);
-        expect(stat.isFile()).to.be.true;
-        expect(stat.size).to.equal(0);
-
-        done();
-      });
-      /* eslint-enable @typescript-eslint/no-non-null-assertion */
+    afterEach(async () => {
+      if (fh !== undefined) {
+        fh.close();
+        await fs.promises.unlink(fh.name);
+        fh = undefined;
+      }
     });
 
-    expect(promise).to.be.undefined;
+    it(`calling tempFile() should create an empty file in ${os.tmpdir()}`, async () => {
+      const promise = tempFile();
+      expect(promise instanceof Promise).to.be.true;
+
+      fh = (await promise) as FileHandle;
+      const stat = await fs.promises.stat(fh.name);
+
+      expect(fh.name.indexOf(os.tmpdir())).to.equal(0);
+      expect(stat.isFile()).to.be.true;
+      expect(stat.size).to.equal(0);
+    });
+
+    it(`calling tempFile({}, callback) should create an empty file in ${os.tmpdir()}`, (done) => {
+      const promise = tempFile({}, (err: Error | null, handle?: FileHandle) => {
+        if (err) throw err;
+
+        expect(handle).to.be.an('object');
+
+        fh = handle;
+        /* eslint-disable @typescript-eslint/no-non-null-assertion */
+        fs.promises.stat(fh!.name).then((stat) => {
+          expect(fh!.name.indexOf(os.tmpdir())).to.equal(0);
+          expect(stat.isFile()).to.be.true;
+          expect(stat.size).to.equal(0);
+
+          done();
+        });
+        /* eslint-enable @typescript-eslint/no-non-null-assertion */
+      });
+
+      expect(promise).to.be.undefined;
+    });
+
+    it(`calling tempFile({pattern:'test-'}) should create an empty file in ${os.tmpdir()} with name matching /test-[\\w\\d_\\-]+$/`, async () => {
+      fh = (await tempFile({pattern: 'test-'})) as FileHandle;
+
+      expect(fh.name).to.match(/\\test-[\w\d_\-]+$/);
+    });
+
+    it(`calling tempFile({pattern:'test-*.txt'}) should create an empty file in ${os.tmpdir()} with name matching /test-[\\w\\d_\\-]+\\.txt$/`, async () => {
+      fh = (await tempFile({pattern: 'test-*.txt'})) as FileHandle;
+
+      expect(fh.name).to.match(/\\test-[\w\d_\-]+\.txt$/);
+    });
+
+    it(`calling tempFile({dir: __dirname}) should create an empty file in ${__dirname}`, async () => {
+      fh = (await tempFile({dir: __dirname})) as FileHandle;
+
+      expect(fh.name.indexOf(__dirname)).to.equal(0);
+    });
   });
 
-  it(`calling tempFile({pattern:'test-'}) should create an empty file in ${os.tmpdir()} with name matching /test-[\\w\\d_\\-]+$/`, async () => {
-    fh = (await tempFile({pattern: 'test-'})) as FileHandle;
+  describe('tempFileOfSize', function () {
+    let fh: FileHandle | undefined = undefined;
 
-    expect(fh.name).to.match(/\\test-[\w\d_\-]+$/);
-  });
+    // beforeEach(() => {});
 
-  it(`calling tempFile({pattern:'test-*.txt'}) should create an empty file in ${os.tmpdir()} with name matching /test-[\\w\\d_\\-]+\\.txt$/`, async () => {
-    fh = (await tempFile({pattern: 'test-*.txt'})) as FileHandle;
+    afterEach(async () => {
+      if (fh !== undefined) {
+        fh.close();
+        await fs.promises.unlink(fh.name);
+        fh = undefined;
+      }
+    });
 
-    expect(fh.name).to.match(/\\test-[\w\d_\-]+\.txt$/);
-  });
+    it(`calling tempFileOfSize({size: '1Mb'}) should create a file of 1Mb`, async () => {
+      const promise = tempFileOfSize({size: '1Mb'});
+      expect(promise instanceof Promise).to.be.true;
 
-  it(`calling tempFile({dir: __dirname}) should create an empty file in ${__dirname}`, async () => {
-    fh = (await tempFile({dir: __dirname})) as FileHandle;
+      fh = (await promise) as FileHandle;
+      const stat = await fs.promises.stat(fh.name);
+      expect(stat.size).to.equal(bytes('1Mb'));
+    });
 
-    expect(fh.name.indexOf(__dirname)).to.equal(0);
-  });
-});
+    it(`calling tempFileOfSize({size: '20Mb'}) should create a file of 20Mb`, async () => {
+      const promise = tempFileOfSize({size: '20Mb'});
+      expect(promise instanceof Promise).to.be.true;
 
-describe('tempFileOfSize', function () {
-  let fh: FileHandle | undefined = undefined;
+      fh = (await promise) as FileHandle;
+      const stat = await fs.promises.stat(fh.name);
+      expect(stat.size).to.equal(bytes('20Mb'));
+    });
 
-  // beforeEach(() => {});
+    it(`calling tempFileOfSize({size: 'sadfds'}) should create a file of 0 bytes`, async () => {
+      const promise = tempFileOfSize({size: 'sadfds'});
+      expect(promise instanceof Promise).to.be.true;
 
-  afterEach(async () => {
-    if (fh !== undefined) {
-      fh.close();
-      await fs.promises.unlink(fh.name);
-      fh = undefined;
-    }
-  });
-
-  it(`calling tempFileOfSize({size: '1Mb'}) should create a file of 1Mb`, async () => {
-    const promise = tempFileOfSize({size: '1Mb'});
-    expect(promise instanceof Promise).to.be.true;
-
-    fh = (await promise) as FileHandle;
-    const stat = await fs.promises.stat(fh.name);
-    expect(stat.size).to.equal(bytes('1Mb'));
-  });
-
-  it(`calling tempFileOfSize({size: '20Mb'}) should create a file of 20Mb`, async () => {
-    const promise = tempFileOfSize({size: '20Mb'});
-    expect(promise instanceof Promise).to.be.true;
-
-    fh = (await promise) as FileHandle;
-    const stat = await fs.promises.stat(fh.name);
-    expect(stat.size).to.equal(bytes('20Mb'));
-  });
-
-  it(`calling tempFileOfSize({size: 'sadfds'}) should create a file of 0 bytes`, async () => {
-    const promise = tempFileOfSize({size: 'sadfds'});
-    expect(promise instanceof Promise).to.be.true;
-
-    fh = (await promise) as FileHandle;
-    const stat = await fs.promises.stat(fh.name);
-    expect(stat.size).to.equal(0);
+      fh = (await promise) as FileHandle;
+      const stat = await fs.promises.stat(fh.name);
+      expect(stat.size).to.equal(0);
+    });
   });
 });
