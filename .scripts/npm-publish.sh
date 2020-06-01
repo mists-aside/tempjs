f1692e982e106d630ad06b9b8cd223e1084691ff
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-06-02 02:29:23 +0300

    +

--- .scripts/npm-publish.sh
+++ .scripts/npm-publish.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 set -xe
 
-# echo "//npmjs.com/:_authToken=$NPMJS_AUTH_TOKEN" > ~/.npmrc
-npm set //npmjs.com/:_authToken $NPMJS_AUTH_TOKEN
+if [[ $TRAVIS_NODE_VERSION == '12' ]] && [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
-npm publish --acccess public
+  # echo "//resistry.npmjs.com/:_authToken=$NPMJS_AUTH_TOKEN" > ~/.npmrc
+  npm config set registry http://registry.npmjs.com
+  npm set //registry.npmjs.com/:_authToken $NPMJS_AUTH_TOKEN
 
-echo 'Congrats on your publication!'
+  # npm install -g npm-cli-login
+  # npm-cli-login login -u $NPMJS_USER -p $NPMJS_PASS -e $NPMJS_MAIL -r http://registry.npmjs.org
+
+  npm publish #--acccess public
+
+  echo 'Congrats on your publication!'
+
+fi
